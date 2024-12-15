f4af140870036cb36676f092edc65619620d0ef9
Ahmed8710 <ahmed.abdelazim@innovs-it.com>
2024-12-15 21:57:47 +0200

    changes

---
+++ src/Wajba.Application.Contracts/Dtos/SitesContact/SiteDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Wajba.Dtos.SitesContact;
+
+public class SiteDto:EntityDto<int>
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string IOSAPPLink { get; set; }
+    public string AndroidAPPLink { get; set; }
+    public string Copyrights { get; set; }
+    public string GoogleMapKey { get; set; }
+    public int Quantity { get; set; }
+    public CurrencyPosition CurrencyPosition { get; set; }
+    public LanguageSwitch LanguageSwitch { get; set; }
+    public int BranchId { get; set; }
+    public int CurrencyId { get; set; }
+    public int LanguageId { get; set; }
+}
