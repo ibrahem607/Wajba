14ff5612aec20570579673f384810b463555c6eb
Ahmed8710 <ahmed.abdelazim@innovs-it.com>
2024-12-16 12:58:00 +0200

    changes

--- src/Wajba.EntityFrameworkCore/Configurations/DineTableConfigurattion.cs
+++ src/Wajba.EntityFrameworkCore/Configurations/DineTableConfigurattion.cs
@@ -7,5 +7,6 @@ public class DineTableConfigurattion : IEntityTypeConfiguration<DineInTable>
     public void Configure(EntityTypeBuilder<DineInTable> builder)
     {
         builder.ToTable("DineInTables");
+
     }
 }
