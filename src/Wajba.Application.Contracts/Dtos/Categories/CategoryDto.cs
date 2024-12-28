ce045f4d1c1c814ef658daddc56c1e55d420dd51
Ahmed8710 <ahmed.abdelazim@innovs-it.com>
2024-12-28 15:44:29 +0200

    changes

--- src/Wajba.Application.Contracts/Dtos/Categories/CategoryDto.cs
+++ src/Wajba.Application.Contracts/Dtos/Categories/CategoryDto.cs
@@ -14,5 +14,5 @@ public class CategoryDto : EntityDto<int>
 }
 public class GetCategoryInput : PagedAndSortedResultRequestDto
 {
-    public string? Filter { get; set; }
+    //public string? Filter { get; set; }
 }
--- src/Wajba.Application/Categories/CategoryAppService.cs
+++ src/Wajba.Application/Categories/CategoryAppService.cs
@@ -66,10 +66,6 @@ public class CategoryAppService : ApplicationService
     public async Task<PagedResultDto<CategoryDto>> GetListAsync(GetCategoryInput input)
     {
         IQueryable<Category> queryable = await _categoryRepository.GetQueryableAsync();
-        queryable = queryable.WhereIf(
-            !string.IsNullOrWhiteSpace(input.Filter),
-            c => c.Name.Contains(input.Filter) || c.Description.Contains(input.Filter)
-        );
         int totalCount = await AsyncExecuter.CountAsync(queryable);
         List<Category> items = await AsyncExecuter.ToListAsync(queryable
             .OrderBy(input.Sorting ?? nameof(Category.Name))
